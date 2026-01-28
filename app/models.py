4087aecdbd4cabc307bbcc0e9214ecea64b9f854
mndrake95 <mandra4a@gmail.com>
2026-01-28 12:54:24 +0500

    modified:   app/models.py 	renamed:    app/docker-compose.yml -> docker-compose.yml

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import uuid
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, Float, DateTime, ForeignKey
+from sqlalchemy import String, Text, Integer, Float, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY, UUID
 from sqlalchemy import func
 import datetime
@@ -12,3 +12,19 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String)
+
+class Chat(Base):
+    __tablename__ = "chats"
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.timezone.utc)
+    messages: Mapped[list["Message"]] = relationship("Message", back_populates="chat", cascade="all, delete")
+
+class Message(Base):
+    __tablename__ = "messages"
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.timezone.utc)
+    chat_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("chats.id"))
+    author_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"))
+    chat: Mapped["Chat"] = relationship("Chat", back_populates="messages")
