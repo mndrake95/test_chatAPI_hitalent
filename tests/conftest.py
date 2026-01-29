04f49fd8394739cf9e7b233911bf1ea5072e0743
mndrake95 <mandra4a@gmail.com>
2026-01-29 13:48:34 +0500

    done fixrures and test, trying to start pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 import pytest
 import asyncio
 
@@ -12,7 +17,7 @@ from app.config import settings
 from app.main import app
 
 
-TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/test_db"
+TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/test_db"
 
 engine_test = create_async_engine(TEST_DATABASE_URL, poolclass=NullPool)
 
@@ -20,6 +25,7 @@ TestingSessionLocal = async_sessionmaker(bind=engine_test, expire_on_commit=Fals
 
 @pytest.fixture(scope="session", autouse=True)
 async def prepare_database():
+    await asyncio.sleep(1)
     async with engine_test.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
@@ -29,16 +35,21 @@ async def prepare_database():
     async with engine_test.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
-@pytest.fixture(scope="session", autouse=True)
-async def override_get_db():
-    app.dependency_overrides[get_db] = override_get_db
+@pytest.fixture(scope="function")
+async def session():
+    async with engine_test.connect() as connection:
+        async with connection.begin() as transaction:
+            async with TestingSessionLocal(bind=connection) as session:
+                yield session
+            await transaction.rollback()
+
+@pytest.fixture(scope="function", autouse=True)
+async def client(session):
+    async def _get_test_db():
+        yield session
+    app.dependency_overrides[get_db] = _get_test_db
     transport = ASGITransport(app=app)
-    async with AsyncClient(base_url="http://test", transport=True) as client:
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
         yield client
         app.dependency_overrides.clear()
 
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,8 @@
 import pytest
-from httpx import AsyncClient, ASGITransport
 from app.main import app
 
-@pytest.mark.asyncio
-async def test_create_chat_success():
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.post("/chats/", json={"title": "My TDD Chat"})
+async def test_create_chat_success(client):
+    response = await client.post("/chats/", json={"title": "My TDD Chat"})
     assert response.status_code == 201
     data = response.json()
     assert data["title"] == "My TDD Chat"
